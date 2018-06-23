4bc5bd44be2ef6bdd403e759c0f2e8a860532faf
Sara S <ssantmier@gmail.com>
2018-06-23 16:04:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ function subtract(a, b) {return a-b}
 function multiply (a, b) {return a*b}
 function divide(a, b) {return a/b}
 
-var n = 10;
-function inc() {number += 1}
-function inc(n) 
-function dec() {number -= 1}
+
+function inc(n) {n += 1}
+function dec(n) {number -= 1}
