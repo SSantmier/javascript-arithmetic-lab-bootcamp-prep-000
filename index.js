399597c42a0996a0c5442988049bc215f0d155d6
Sara S <ssantmier@gmail.com>
2018-06-23 16:10:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ function divide(a, b) {return a/b}
 function inc(n) {n += 1}
 function dec(n) {n -= 1}
 
-function makeInt(n) {return parsInt(n, 10)}
+function makeInt(n) {return parseInt(n, 10)}
 function preserveDecimal(n) {return parseFloat(n)}
